f75fa8ce28545ed7fbae9b8fc94a0a368e2897cb
Joe Takara <anuchit.boonsom@gmail.com>
2026-03-30 03:29:42 +0700

    refactor(tooling): Deduplicate generator config

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,5 +1,52 @@
 import type { PlopTypes } from "@turbo/gen";
 
+const TEMPLATE_FILES = [
+  { ext: "css", path: "packages/styles/src/components/{{dashCase name}}.css", file: "component.css.hbs" },
+  {
+    ext: "tsx",
+    path: "packages/react/src/components/{{dashCase name}}/{{pascalCase name}}.tsx",
+    file: "Component.tsx.hbs",
+  },
+  {
+    ext: "variants",
+    path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.variants.ts",
+    file: "component.variants.ts.hbs",
+  },
+  {
+    ext: "test",
+    path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.test.tsx",
+    file: "component.test.tsx.hbs",
+  },
+  {
+    ext: "stories",
+    path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.stories.tsx",
+    file: "component.stories.tsx.hbs",
+  },
+  { ext: "index", path: "packages/react/src/components/{{dashCase name}}/index.ts", file: "index.ts.hbs" },
+];
+
+function createActions(templateDir: string): PlopTypes.ActionType[] {
+  const addActions: PlopTypes.ActionType[] = TEMPLATE_FILES.map(({ path, file }) => ({
+    type: "add" as const,
+    path,
+    templateFile: `${templateDir}/${file}`,
+  }));
+
+  return [
+    ...addActions,
+    {
+      type: "append",
+      path: "packages/styles/src/components/index.css",
+      template: '@import "./{{dashCase name}}.css";',
+    },
+    {
+      type: "append",
+      path: "packages/react/src/components/index.ts",
+      template: 'export * from "./{{dashCase name}}";',
+    },
+  ];
+}
+
 function generator(plop: PlopTypes.NodePlopAPI): void {
   plop.setGenerator("display-component", {
     description: "Display-only component (native HTML element)",
@@ -10,48 +57,7 @@ function generator(plop: PlopTypes.NodePlopAPI): void {
         message: "Component name (PascalCase, e.g. Surface):",
       },
     ],
-    actions: [
-      {
-        type: "add",
-        path: "packages/styles/src/components/{{dashCase name}}.css",
-        templateFile: "display-component/component.css.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{pascalCase name}}.tsx",
-        templateFile: "display-component/Component.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.variants.ts",
-        templateFile: "display-component/component.variants.ts.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.test.tsx",
-        templateFile: "display-component/component.test.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.stories.tsx",
-        templateFile: "display-component/component.stories.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/index.ts",
-        templateFile: "display-component/index.ts.hbs",
-      },
-      {
-        type: "append",
-        path: "packages/styles/src/components/index.css",
-        template: '@import "./{{dashCase name}}.css";',
-      },
-      {
-        type: "append",
-        path: "packages/react/src/components/index.ts",
-        template: 'export * from "./{{dashCase name}}";',
-      },
-    ],
+    actions: createActions("display-component"),
   });
 
   plop.setGenerator("interactive-component", {
@@ -63,48 +69,7 @@ function generator(plop: PlopTypes.NodePlopAPI): void {
         message: "Component name (PascalCase, e.g. Link):",
       },
     ],
-    actions: [
-      {
-        type: "add",
-        path: "packages/styles/src/components/{{dashCase name}}.css",
-        templateFile: "interactive-component/component.css.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{pascalCase name}}.tsx",
-        templateFile: "interactive-component/Component.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.variants.ts",
-        templateFile: "interactive-component/component.variants.ts.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.test.tsx",
-        templateFile: "interactive-component/component.test.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/{{dashCase name}}.stories.tsx",
-        templateFile: "interactive-component/component.stories.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "packages/react/src/components/{{dashCase name}}/index.ts",
-        templateFile: "interactive-component/index.ts.hbs",
-      },
-      {
-        type: "append",
-        path: "packages/styles/src/components/index.css",
-        template: '@import "./{{dashCase name}}.css";',
-      },
-      {
-        type: "append",
-        path: "packages/react/src/components/index.ts",
-        template: 'export * from "./{{dashCase name}}";',
-      },
-    ],
+    actions: createActions("interactive-component"),
   });
 }
 
