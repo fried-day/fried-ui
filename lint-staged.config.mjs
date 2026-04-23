b6dbdb204d06b5a515c75347b60353c7fe098734
Joe Takara <anuchit.boonsom@gmail.com>
2026-04-23 10:30:37 +0700

    chore(format): Expand to jsonc yml yaml files

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import("lint-staged").Config} */
 const CONFIG = {
-  "*.{css,md,mdx}": ["prettier --write"],
+  "*.{css,md,mdx,json,jsonc,yml,yaml}": ["prettier --write"],
   "*.{ts,tsx,js,jsx,mjs}": ["prettier --write", "eslint --fix"],
   "package.json": ["sort-package-json", "prettier --write"],
 };
